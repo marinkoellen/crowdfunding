d59af44577f31c9444355e81aca51db56a4dc07b
marinkoellen <ellen.marinko1@gmail.com>
2020-09-19 11:20:09 +0800

    used hayleys dummy data

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App(){
             <HomePage />
           </Route>
 
-          <Route path="/projects">
+          <Route path="/project">
             <ProjectPage />
           </Route>
 
