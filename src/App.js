d7684d688cd4e780ae2ff287cd7fda876cf1ddfc
marinkoellen <ellen.marinko1@gmail.com>
2020-09-19 10:45:14 +0800

    added nav bar and project and home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Nav from "./components/Nav/Nav";
+import HomePage from "./pages/HomePage";
+import ProjectPage from "./pages/ProjectPage";
+
+
+export default function App(){
+  return(
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+        <Route exact path="/">
+            <HomePage />
+          </Route>
+
+          <Route path="/projects">
+            <ProjectPage />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
+  )
 }
-
-export default App;
