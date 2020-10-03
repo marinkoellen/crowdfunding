77a656d91fcff981addfe27e4f059cd912e3fc47
marinkoellen <ellen.marinko1@gmail.com>
2020-10-03 10:51:51 +0800

    working user

--- src/App.js
+++ src/App.js
@@ -1,27 +1,72 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css"
+import "./App.css";
 import Nav from "./components/Nav/Nav";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import HomePage from "./pages/HomePage";
 import ProjectPage from "./pages/ProjectPage";
+import ProjectEditPage from "./pages/EditProjectPage";
+import TestPage from "./pages/TestPage";
 
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import PledgePage from "./pages/PledgePage";
 
-export default function App(){
-  return(
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-        <Route exact path="/">
-            <HomePage />
-          </Route>
+import CreateProjectPage from "./pages/CreateProjectPage";
+import CreateUserPage from "./pages/CreateUserPage";
+import SideBar from "./components/SideBar/SideBar.jsx";
+import "./components/SideBar/SideBar.css";
 
-          <Route path="/projects/:id">
-            <ProjectPage />
-          </Route>
-
-        </Switch>
+export default function App() {
+  return (
+    <div id="App">
+      <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+      <div id="page-wrap">
+        <h1>Nibble 🍔🍕</h1>
       </div>
-    </Router>
-  )
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+
+            <Route exact path="/profile/">
+              <CreateUserPage />
+            </Route>
+
+            <Route exact path="/test/">
+              <TestPage />
+            </Route>
+
+            <PrivateRoute
+              path="/project-create/"
+              comp={CreateProjectPage}
+            ></PrivateRoute>
+
+            <Route exact path="/pledge/:id">
+              <PledgePage />
+            </Route>
+
+            {/* <PrivateRoute path="/pledge/:id" comp={PledgePage}></PrivateRoute> */}
+
+            <PrivateRoute path="/userprofile" comp={ProfilePage}></PrivateRoute>
+
+            <Route path="/projects/edit/:id">
+              <ProjectEditPage />
+            </Route>
+
+            <Route path="/projects/:id">
+              <ProjectPage />
+            </Route>
+
+            <Route path="/login">
+              <LoginPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
